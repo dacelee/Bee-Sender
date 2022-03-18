50409f82b8b65da10efed0d9c53bfacbf31074a6
Ace lee <38158900+dacelee@users.noreply.github.com>
2022-03-18 20:50:51 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,4 +2,4 @@
 // change this address to your own address , a half transfer fee will send to this address. 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var FeeReciever = "0xEAc8555E83919Cedc065849b0FDc843bC03EFd4d";
+var FeeReciever = "0x25aa9327b1262b28837b50605CE57B82c0a45370";
